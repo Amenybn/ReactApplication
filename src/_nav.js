3f3ab89615ad6c92161de1ebe6f9a750fbe002ad
AmenyBn <amanibennaceur1234@gmail.com>
2024-07-23 21:35:41 +0200

    remove unnecessary files

--- src/_nav.js
+++ src/_nav.js
@@ -26,26 +26,7 @@ const _nav = [
       text: 'NEW',
     },
   },
-  {
-    component: CNavTitle,
-    name: 'Theme',
-  },
-  {
-    component: CNavItem,
-    name: 'Colors',
-    to: '/theme/colors',
-    icon: <CIcon icon={cilDrop} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavItem,
-    name: 'Typography',
-    to: '/theme/typography',
-    icon: <CIcon icon={cilPencil} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavTitle,
-    name: 'Components',
-  },
+ 
   {
     component: CNavGroup,
     name: 'Base',
@@ -107,10 +88,6 @@ const _nav = [
     ],
   },
  
-  {
-    component: CNavTitle,
-    name: 'Extras',
-  },
   {
     component: CNavGroup,
     name: 'Pages',
